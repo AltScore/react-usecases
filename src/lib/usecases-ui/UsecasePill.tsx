5bcfc4b3319fe1d8ebfb089c7a9a7de94f122728
jgramsch <jigramsch@uc.cl>
2024-03-18 19:11:09 -0300

    bigger pills

--- src/lib/usecases-ui/UsecasePill.tsx
+++ src/lib/usecases-ui/UsecasePill.tsx
@@ -1,37 +1,31 @@
-import {Card, CardActionArea, CardContent, Typography} from "@mui/material";
+import { Card, CardActionArea, CardContent, Typography } from "@mui/material";
 import React from "react";
 
-const DefaultUsecasePill = (
-    {
-        usecase,
-        onUsecaseClicked,
-    }: DefaultUsecasePillProps
-) => {
-    return <Card
-        sx={{
-            Width: 192,
-        }}
+const DefaultUsecasePill = ({
+  usecase,
+  onUsecaseClicked,
+}: DefaultUsecasePillProps) => {
+  return (
+    <Card
+      sx={{
+        width: 256,
+        height: 256,
+      }}
     >
-        <CardActionArea
-            onClick={() => onUsecaseClicked(usecase)}
-        >
-            <CardContent>
-                <Typography variant={"h6"}>
-                    {usecase.name}
-                </Typography>
-                <Typography variant={"body2"}>
-                    {usecase.description}
-                </Typography>
-            </CardContent>
-        </CardActionArea>
+      <CardActionArea onClick={() => onUsecaseClicked(usecase)}>
+        <CardContent>
+          <Typography variant={"h6"}>{usecase.name}</Typography>
+          <Typography variant={"body2"}>{usecase.description}</Typography>
+        </CardContent>
+      </CardActionArea>
     </Card>
-}
-export {DefaultUsecasePill};
+  );
+};
+export { DefaultUsecasePill };
 
 export interface UsecasePillProps {
-    usecase: any;
-    onUsecaseClicked: (usecase: any) => void;
+  usecase: any;
+  onUsecaseClicked: (usecase: any) => void;
 }
 
-interface DefaultUsecasePillProps extends UsecasePillProps {
-}
+interface DefaultUsecasePillProps extends UsecasePillProps {}
